fd97899d9effd0d9946515f080804959585cacd6
trojvn <mrx.trojan@gmail.com>
2025-05-07 12:31:43 +0300

    refactor

--- ncwrapper/__init__.py
+++ ncwrapper/__init__.py
@@ -11,16 +11,43 @@ class NextCloud(BaseClient):
         super().__init__(base_url, {"OCS-APIRequest": "true"}, auth=(user, pswd))
         self.__config = ClientConfig(3, 900, 3, 3)
 
-    def mkdir(self, dir_name: str) -> bool:
+    def mkdir(self, dir_path: str) -> bool:
+        """
+        Create a directory on the NextCloud server.
+
+        Args:
+            dir_path (str): The path to the directory to create.
+            example:
+                dir_path = "/path/to/directory"
+
+        Returns:
+            bool: True if the directory was created successfully, False otherwise.
+        """
+        if not dir_path.startswith("/"):
+            dir_path = f"/{dir_path}"
         return self._request(
             "MKCOL",
-            f"/remote.php/webdav/{dir_name}",
+            f"/remote.php/webdav{dir_path}",
             config=self.__config,
         ).status_code in (201, 405)
 
-    def upload_file(self, file: BufferedReader, file_name: str) -> bool:
+    def upload_file(self, file: BufferedReader, file_path: str) -> bool:
+        """
+        Upload a file to the NextCloud server.
+
+        Args:
+            file (BufferedReader): The file to upload.
+            file_path (str): The path to the file to upload.
+            example:
+                file_path = "/path/to/file.txt"
+
+        Returns:
+            bool: True if the file was uploaded successfully, False otherwise.
+        """
+        if not file_path.startswith("/"):
+            file_path = f"/{file_path}"
         return self._put(
-            f"/remote.php/webdav/{file_name}",
+            f"/remote.php/webdav{file_path}",
             config=self.__config,
             content=file,
         ).status_code in (201,)
@@ -31,6 +58,23 @@ class NextCloud(BaseClient):
         share_type: int = 3,
         permissions: int = 1,
     ) -> str:
+        """
+        Share a file on the NextCloud server.
+
+        Args:
+            file_path (str): The path to the file to share.
+            share_type (int): The type of share to create.
+            permissions (int): The permissions to set on the share.
+            example:
+                file_path = "/path/to/file.txt"
+                share_type = 3
+                permissions = 1
+
+        Returns:
+            str: The URL of the shared file.
+        """
+        if not file_path.startswith("/"):
+            file_path = f"/{file_path}"
         r = self._post(
             url="/ocs/v2.php/apps/files_sharing/api/v1/shares",
             config=self.__config,
--- ncwrapper/async_.py
+++ ncwrapper/async_.py
@@ -10,20 +10,47 @@ class AsyncNextCloud(BaseAsyncClient):
         super().__init__(base_url, {"OCS-APIRequest": "true"}, auth=(user, pswd))
         self.__config = AsyncClientConfig(3, ClientTimeout(900), 3, 3)
 
-    async def mkdir(self, dir_name: str) -> bool:
+    async def mkdir(self, dir_path: str) -> bool:
+        """
+        Create a directory on the NextCloud server.
+
+        Args:
+            dir_path (str): The path to the directory to create.
+            example:
+                dir_path = "path/to/directory"
+
+        Returns:
+            bool: True if the directory was created successfully, False otherwise.
+        """
+        if not dir_path.startswith("/"):
+            dir_path = f"/{dir_path}"
         return (
             await self._request(
                 method="MKCOL",
                 config=self.__config,
-                url=f"/remote.php/webdav/{dir_name}",
+                url=f"/remote.php/webdav{dir_path}",
             )
         ).status in (201, 405)
 
-    async def upload_file(self, file: BufferedReader, file_name: str) -> bool:
+    async def upload_file(self, file: BufferedReader, file_path: str) -> bool:
+        """
+        Upload a file to the NextCloud server.
+
+        Args:
+            file (BufferedReader): The file to upload.
+            file_path (str): The path to the file to upload.
+            example:
+                file_path = "path/to/file.txt"
+
+        Returns:
+            bool: True if the file was uploaded successfully, False otherwise.
+        """
+        if not file_path.startswith("/"):
+            file_path = f"/{file_path}"
         return (
             await self._put(
-                url=f"/remote.php/webdav/{file_name}",
                 config=self.__config,
+                url=f"/remote.php/webdav{file_path}",
                 data=file,
             )
         ).status in (201,)
@@ -34,6 +61,23 @@ class AsyncNextCloud(BaseAsyncClient):
         share_type: int = 3,
         permissions: int = 1,
     ) -> str:
+        """
+        Share a file on the NextCloud server.
+
+        Args:
+            file_path (str): The path to the file to share.
+            share_type (int): The type of share to create.
+            permissions (int): The permissions to set on the share.
+            example:
+                file_path = "path/to/file.txt"
+                share_type = 3
+                permissions = 1
+
+        Returns:
+            str: The URL of the shared file.
+        """
+        if not file_path.startswith("/"):
+            file_path = f"/{file_path}"
         r = await self._post(
             url="/ocs/v2.php/apps/files_sharing/api/v1/shares",
             config=self.__config,
